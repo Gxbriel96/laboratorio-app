e09956ab29d3ab498a41e525896c68d644de5f5c
Gxbriel96 <alexandroggm@gmail.com>
2023-12-12 11:56:54 -0400

    se creó el input dinámico y formulario base de los datos del cliente

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import { CostumersForm } from './components/CostumersForm'
 
 const App = (): JSX.Element => {
   return (
-    <CostumersForm/>
+    <div className='bg-gradient-to-r from-blue-400 to-blue-100 via-blue-50 from-blue-100 to-blue-400 h-screen w-screen'>
+      <CostumersForm/>
+    </div>
   )
 }
 
--- src/components/CostumersForm.tsx
+++ src/components/CostumersForm.tsx
@@ -1,14 +1,29 @@
 import { CostumerInput } from './CostumerInput'
 
-export const CostumersForm = () => {
+export const CostumersForm: React.FC = () => {
   return (
-        <div className='bg-gradient-to-r from-blue-400 to-blue-100 via-blue-50 from-blue-100 to-blue-400 h-screen w-screen' >
-            <h1>FORMULARIO DE CLIENTES</h1>
-            <form>
-                <div>
-                    <CostumerInput/>
-                </div>
-            </form>
+    <div className='flex flex-col border rounded items-center max-w-md mx-auto my-0 bg-gray-50 p-4 sm:max-w-lg md:max-w-xl lg:max-w-2xl xl:max-w-3xl'>
+        <div className='bg-gray-100 w-full p-2'>
+            <h1 className='text-2xl font-semibold text-gray-600'>Información del Cliente</h1>
         </div>
+        <form className='w-full'>
+            <div className='my-2'>
+                <CostumerInput titleInput='Nombre' name='Nombre' typeInput='text'/>
+            </div>
+            <div className='my-2'>
+                <CostumerInput titleInput='Apellido' name='Apellido' typeInput='text'/>
+            </div>
+            <div className='my-2'>
+                <CostumerInput titleInput='Correo Electrónico' name='email' typeInput='email'/>
+            </div>
+            <div className='my-2'>
+                <CostumerInput titleInput='Edad' name='age' typeInput='number'/>
+            </div>
+            <button className='w-24 py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-600'>
+                Guardar
+            </button>
+
+        </form>
+    </div>
   )
 }
