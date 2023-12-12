d6a27f5873a1a233c4ea7fbfca4b72e0863713dd
Gxbriel96 <alexandroggm@gmail.com>
2023-12-12 13:08:11 -0400

    agregado y configurado de prettier junto al linter

--- src/components/CostumerInput.tsx
+++ src/components/CostumerInput.tsx
@@ -1,19 +1,30 @@
 interface Cliente {
-  id?: number
-  name?: string
-  age?: number
+	id?: number
+	name?: string
+	age?: number
 }
 
 interface ClienteProps extends Cliente {
-  titleInput: string
-  typeInput: string
+	titleInput: string
+	typeInput: string
 }
 
-export const CostumerInput: React.FC<ClienteProps> = ({ name, titleInput, typeInput }) => {
-  return (
-    <div className='flex flex-col'>
-      <label htmlFor={name} className="text-sm font-medium text-gray-700 mb-2">{titleInput}</label>
-      <input id={name} type={typeInput} className="px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder={titleInput} />
-    </div>
-  )
+export const CostumerInput: React.FC<ClienteProps> = ({
+	name,
+	titleInput,
+	typeInput,
+}) => {
+	return (
+		<div className='flex flex-col'>
+			<label htmlFor={name} className='text-sm font-medium text-gray-700 mb-2'>
+				{titleInput}
+			</label>
+			<input
+				id={name}
+				type={typeInput}
+				className='px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500'
+				placeholder={titleInput}
+			/>
+		</div>
+	)
 }
--- src/components/CostumersForm.tsx
+++ src/components/CostumersForm.tsx
@@ -1,29 +1,38 @@
 import { CostumerInput } from './CostumerInput'
 
 export const CostumersForm: React.FC = () => {
-  return (
-    <div className='flex flex-col border rounded items-center max-w-md mx-auto my-0 bg-gray-50 p-4 sm:max-w-lg md:max-w-xl lg:max-w-2xl xl:max-w-3xl'>
-        <div className='bg-gray-100 w-full p-2'>
-            <h1 className='text-2xl font-semibold text-gray-600'>Información del Cliente</h1>
-        </div>
-        <form className='w-full'>
-            <div className='my-2'>
-                <CostumerInput titleInput='Nombre' name='Nombre' typeInput='text'/>
-            </div>
-            <div className='my-2'>
-                <CostumerInput titleInput='Apellido' name='Apellido' typeInput='text'/>
-            </div>
-            <div className='my-2'>
-                <CostumerInput titleInput='Correo Electrónico' name='email' typeInput='email'/>
-            </div>
-            <div className='my-2'>
-                <CostumerInput titleInput='Edad' name='age' typeInput='number'/>
-            </div>
-            <button className='w-24 py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-600'>
-                Guardar
-            </button>
-
-        </form>
-    </div>
-  )
+	return (
+		<div className='flex flex-col border rounded items-center max-w-md mx-auto my-0 bg-gray-50 p-4 sm:max-w-lg md:max-w-xl lg:max-w-2xl xl:max-w-3xl'>
+			<div className='bg-gray-100 w-full p-2'>
+				<h1 className='text-2xl font-semibold text-gray-600'>
+					Información del Cliente
+				</h1>
+			</div>
+			<form className='w-full'>
+				<div className='my-2'>
+					<CostumerInput titleInput='Nombre' name='Nombre' typeInput='text' />
+				</div>
+				<div className='my-2'>
+					<CostumerInput
+						titleInput='Apellido'
+						name='Apellido'
+						typeInput='text'
+					/>
+				</div>
+				<div className='my-2'>
+					<CostumerInput
+						titleInput='Correo Electrónico'
+						name='email'
+						typeInput='email'
+					/>
+				</div>
+				<div className='my-2'>
+					<CostumerInput titleInput='Edad' name='age' typeInput='number' />
+				</div>
+				<button className='w-24 py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-600'>
+					Guardar
+				</button>
+			</form>
+		</div>
+	)
 }
